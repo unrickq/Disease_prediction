98438c4989f94e5c855360b6218203e3ca258a78
jose <57227710+unrickq@users.noreply.github.com>
2021-04-22 20:29:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/diseaseprediction/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.diseaseprediction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.diseaseprediction.ui.account.AccountFragment;
+import com.example.diseaseprediction.ui.alert.AlertFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Set toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_account, R.id.nav_consultationList, R.id.nav_predictionList,
+                R.id.nav_settings, R.id.nav_about, R.id.nav_alert)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        //set toolbar icon
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.alert) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            //transaction.remove();
+            this.getFragmentManager().popBackStack();
+            transaction.add(R.id.nav_host_fragment, new AccountFragment());
+            //transaction.addToBackStack(null);
+            transaction.commit();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setActionBarTitle(String title) {
+        TextView t = (TextView) findViewById(R.id.toolbar_title) ;
+        t.setText(title) ;
+    }
+    public void setIconToolbar(){
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger);
+    }
+}
