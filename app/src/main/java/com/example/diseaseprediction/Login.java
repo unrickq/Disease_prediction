cc21519e965cd47f6c9f2a87d778c4a8b88a7d39
jose <57227710+unrickq@users.noreply.github.com>
2021-06-12 09:14:44 +0700

    add data

--- app/src/main/java/com/example/diseaseprediction/Login.java
+++ app/src/main/java/com/example/diseaseprediction/Login.java
@@ -43,7 +43,7 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_login);
 
         // Configure Google Sign In
